3461d7a90d6e7fa3b4129fa159d80a4b398944c4
Alexander Bazo <alexanderbazo@googlemail.com>
2020-12-16 09:49:56 +0100

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,16 @@
+/*eslint-env browser */
+
+/**
+ * How to use Open Trivia Database API
+ * 
+ * Docs & Helper: https://opentdb.com/api_config.php
+ * Example: https://opentdb.com/api.php?amount=1&category=10&type=multiple (returns one multiple choice questions from the "books" category with random difficulty)
+ * Categories: Books (10), Film (11), Video Games (15), Computers (18)
+ * 
+ */
+
+function init() {
+    console.log("### Starting Random Quiz App ###");
+}
+
+init();
